e87594db41dff43c259d55b74615589d9579fd97
walter <vuongtran79991@gmail.com>
2019-02-25 14:23:58 +0700

    Relationship create

--- app/API.php
+++ app/API.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use  App\Review;
 class API extends Model
 {
-    //
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Review.php
+++ app/Review.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\API;
 class Review extends Model
 {
-    //
+    public function product(){
+        return $this->belongsTo(API::class);
+    }
 }
